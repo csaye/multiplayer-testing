694893313509684c3d6cde76da3f54b27f566b71
Cooper Saye <coopersaye@gmail.com>
2020-09-29 09:45:29 -0700

    Fix button activation.

--- Assets/Lobby/Scripts/LobbyNetworkManagerHUD.cs
+++ Assets/Lobby/Scripts/LobbyNetworkManagerHUD.cs
@@ -8,60 +8,51 @@ namespace MultiplayerTesting
         [Header("References")]
         [SerializeField] private LobbyNetworkManager manager = null;
         [SerializeField] private TMP_InputField addressInput = null;
-        [SerializeField] private GameObject joinButton = null, hostButton = null, backButton = null, goButton = null;
-
-        public void StartHost()
-        {
-            manager.StartHost();
-            Go();
-        }
-
-        public void StopHost()
-        {
-            manager.StopHost();
-        }
-
-        public void StartClient()
-        {
-            string address = addressInput.text;
-            if (string.IsNullOrWhiteSpace(address)) return;
-            manager.networkAddress = address;
-            manager.StartClient();
-            Go();
-        }
-
-        public void StopClient()
-        {
-            manager.StopClient();
-        }
+        [SerializeField] private GameObject joinButton = null, hostButton = null, joinBackButton = null, hostBackButton = null, goButton = null;
 
         public void Join()
         {
             joinButton.SetActive(false);
             hostButton.SetActive(false);
             addressInput.gameObject.SetActive(true);
-            backButton.SetActive(true);
+            joinBackButton.SetActive(true);
             goButton.SetActive(true);
         }
 
-        public void Back()
+        public void Host()
         {
-            StopHost();
-            StopClient();
+            joinButton.SetActive(false);
+            hostButton.SetActive(false);
+            hostBackButton.SetActive(true);
+            // manager.StartHost();
+        }
+
+        public void JoinBack()
+        {
             joinButton.SetActive(true);
             hostButton.SetActive(true);
             addressInput.gameObject.SetActive(false);
-            backButton.SetActive(false);
+            joinBackButton.SetActive(false);
             goButton.SetActive(false);
+            // manager.StopClient();
+        }
+
+        public void HostBack()
+        {
+            joinButton.SetActive(true);
+            hostButton.SetActive(true);
+            hostBackButton.SetActive(false);
+            // manager.StopHost();
         }
 
         public void Go()
         {
-            joinButton.SetActive(false);
-            hostButton.SetActive(false);
+            string address = addressInput.text;
+            if (string.IsNullOrWhiteSpace(address)) return;
             addressInput.gameObject.SetActive(false);
-            backButton.SetActive(true);
             goButton.SetActive(false);
+            // manager.networkAddress = address;
+            // manager.StartClient();
         }
     }
 }
